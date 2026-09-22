365a1de938779649b6dc9b47ee68218e61ee7400
darkace101 <darkace052@gmail.com>
2026-09-22 20:41:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+// Automatically update footer copyright year
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// Simple smooth console log confirmation
+console.log("Victor's Portfolio loaded successfully!");
